831b60fd7b2dab8355d6161ba11f7ba06c9b7016
H82M19\Student <Student>
2026-01-24 13:34:49 +0700

    part1_js_baitap1_cau01

---
+++ javascript/Bai1_cau1.js
@@ -0,0 +1,16 @@
+
+const Mark = {
+    mass: 60,
+    height: 180
+}
+
+const John = {
+    mass: 60,
+    height: 180
+}
+
+
+const calculator = () => {
+    const BMI = mass / (height * height)
+    return BMI;
+}
