ec661ad4cf182dbaf317191f9f39f1bbe76bb1d5
H82M19\Student <Student>
2026-01-24 13:47:53 +0700

    part1_js_baitap1_cau02

--- javascript/Bai1_cau1.js
+++ javascript/Bai1_cau1.js
@@ -1,16 +1,20 @@
 
 const Mark = {
-    mass: 60,
-    height: 180
+    weight: 78,
+    height: 169
 }
 
 const John = {
-    mass: 60,
-    height: 180
+    weight: 92,
+    height: 195
 }
 
 
-const calculator = () => {
-    const BMI = mass / (height * height)
+const calculator = (data) => {
+    const mass = data.weight * data.height
+    const BMI = mass / (data.height * data.height)
     return BMI;
 }
+
+console.log ("Can nang cua john la: ", calculator(John))
+console.log ("Can nang cua Mark la: ", calculator(Mark))
