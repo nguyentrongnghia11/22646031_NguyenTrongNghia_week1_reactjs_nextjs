4d75513308dc883f3691e7e8576b00a0079dfc75
H82M19\Student <Student>
2026-01-31 13:32:38 +0700

    part2_react_baitap1

--- react-css/src/App.jsx
+++ react-css/src/App.jsx
@@ -7,7 +7,9 @@ import Button from './components/Button';
 import Alert from './components/Alert';
 import LoginForm from './components/LoginForm';
 import ProductList from './components/ProductList';
-
+import Header from './components/Header'
+import CardInfor from './components/CardInfor'
+import Footer from './components/Footer'
 function App() {
   const [alert, setAlert] = useState({ type: '', message: '' });
 
@@ -22,7 +24,7 @@ function App() {
 
   return (
     <>
-      <ProductList />
+      {/* <ProductList />
       <ProductCard />
       <div style={{ marginTop: 24, display: 'flex', gap: 12, justifyContent: 'center' }}>
         <Button type="success" onClick={() => handleShowAlert('success')}>Success</Button>
@@ -30,7 +32,11 @@ function App() {
         <Button type="danger" onClick={() => handleShowAlert('error')}>Error</Button>
       </div>
       <Alert type={alert.type} message={alert.message} />
-      <LoginForm />
+      <LoginForm /> */}
+
+      <Header/>
+      <CardInfor name={"Nguyen Trong Nghia"} age={22} mssv={22646031}/>
+      <Footer/>
     </>
   )
 }
---
+++ react-css/src/components/CardInfor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CardInfor({ name, age, mssv }) {
+    return (<>
+
+        <div>Ho va ten: {name}</div>
+        <div>Tuoi: {age}</div>
+        <div>Ma so sinh vien: {mssv}</div></>
+    )
+}
+
+export default CardInfor
